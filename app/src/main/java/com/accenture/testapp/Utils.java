b2121b8eb5c50ca54cb6fbe59b03011cf21d74eb
peiyi.liu <peiyi_pl_liu@cathaypacific.com>
2020-06-04 16:26:37 +0800

    first commit

---
+++ app/src/main/java/com/accenture/testapp/Utils.java
@@ -0,0 +1,12 @@
+package com.accenture.testapp;
+
+/**
+ * @Author peiyi.liu
+ * @Date 6/4/2020 4:06 PM
+ */
+public class Utils {
+
+    public static String getLibName() {
+        return "Hi, I'm from TestApp's Utils!";
+    }
+}
